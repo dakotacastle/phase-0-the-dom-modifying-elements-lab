d7d337c0081ae03b48f5abe49cbba8476a6497d6
Dakota Castle <barrettcastle@gmail.com>
2021-11-13 15:17:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const b = document.querySelector("main#main");
+b.parentElement.removeChild(b);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.textContent =  "Dakota is the champion";
+// const victory = document.getElementById("victory");
+// victory.innerHTML = "Dakota is the champion";
